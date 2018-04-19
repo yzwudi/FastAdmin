b0f7125930989e96630995dbd4be504bb8282c2e
yuning <yuning@chemanman.com>
2018-04-19 12:52:47 +0800

    xirr

--- application/extra/addons.php
+++ application/extra/addons.php
@@ -4,16 +4,8 @@ return array (
   'autoload' => false,
   'hooks' => 
   array (
-    'upload_config_init' => 
-    array (
-      0 => 'upyun',
-    ),
   ),
   'route' => 
   array (
-    '/example$' => 'example/index/index',
-    '/example/d/[:name]' => 'example/demo/index',
-    '/example/d1/[:name]' => 'example/demo/demo1',
-    '/example/d2/[:name]' => 'example/demo/demo2',
   ),
 );
